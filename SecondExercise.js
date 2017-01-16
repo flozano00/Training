5654236fb2d9aa37709a7431b37f4c9e0c61a3a2
Frank Lozano <flozano00@gmail.com>
2017-01-16 13:49:08 -0500

    This is the Next Version I changed called "Second Exercise"

---
+++ SecondExercise.js
@@ -0,0 +1,39 @@
+//Second Try !
+
+//Subtraction
+function yearOfBirth(x){
+	return 2016 - x;
+	};
+var age = yearOfBirth(1989);
+console.log(age);
+
+//Addition
+function addition(x, y){
+	return (x + y);
+};
+var total = addition(5, 20);
+console.log(total);
+
+//Multiplication
+function multiplication(x, y){
+	return (x * y);
+}
+var multiply = multiplication(10, 5);
+console.log(multiply);
+
+//Division
+function division (x, y){
+	return (x / y);
+};
+var divide = division(10, 5);
+console.log(divide);
+
+//Modulus(Division Remainder)
+function Mod(x, y){
+	return (x % y);
+}
+var remainder = mod(10,4);
+console.log(remainder);
+
+
+
