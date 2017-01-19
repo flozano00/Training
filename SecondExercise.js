bcbde6532cdbd41a8f6be5cbe4922838dedcd5d3
Frank Lozano <flozano00@gmail.com>
2017-01-19 17:49:09 -0500

    Deletion

---
+++ SecondExercise.js
@@ -0,0 +1,41 @@
+//Second Try !
+
+//Subtraction
+function subtraction(x){
+	return 2016 - x;
+	};
+var age = subtraction(1989);
+console.log(age);
+
+//Addition
+function addition(x, y){
+	return (x + y);
+};
+var total = addition(5, 20);
+console.log(total);
+
+//Multiplication
+function multipy(x, y){
+	return (x * y);
+}
+var multiply = multiplication(10, 5);
+console.log(multiply);
+
+//Division
+function division (x, y){
+	return (x / y);
+};
+var divide = division(10, 5);
+console.log(divide);
+
+//Modulus(Division Remainder)
+function remain(x, y){
+	var sub = x - y;
+	return sub % 5;
+}
+
+var remainder = remain(22, 3);
+console.log(remainder);
+
+
+
