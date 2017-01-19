e12eb4fdf5df3598ccdca1d0c0786fbd25565fdd
Frank Lozano <flozano00@gmail.com>
2017-01-19 18:04:08 -0500

    These functions match your Test file.

--- Exercise.js
+++ Exercise.js
@@ -1,41 +1,23 @@
-
-
-//Subtraction
-function subtraction(x){
-	return 2016 - x;
-	};
-var age = subtraction(1989);
-console.log(age);
-
-//Addition
-function addition(x, y){
-	return (x + y);
+//Add
+function add(x,y){
+	return x + y;
 };
-var total = addition(5, 20);
-console.log(total);
-
+//subtract
+function subtract(x, y){
+	return (x - y);
+};
 //Multiplication
-function multiplication(x, y){
+function multiply(x, y){
 	return (x * y);
-}
-var multiply = multiplication(10, 5);
-console.log(multiply);
-
+};
 //Division
-function division (x, y){
+function divide (x, y){
 	return (x / y);
 };
-var divide = division(10, 5);
-console.log(divide);
-
 //Modulus(Division Remainder)
-function remain(x, y){
-	var sub = x - y;
-	return sub % 5;
-}
-
-var remainder = remain(22, 3);
-console.log(remainder);
+function mod(x, y){
+	return x % y;
+};
 
 
 
---
+++ Test.js
@@ -0,0 +1,19 @@
+console.log(add(8,2)); // => 10
+console.log(add(-5, 7)); // => 2
+console.log(add(0, 28)); // => 28
+
+console.log(subtract(5,3)); // => 2
+console.log(subtract(-2, -7)); // => 5
+console.log(subtract(132, 276)); // => -144
+
+console.log(multiply(10, 12)); // => 120
+console.log(multiply(-2, 8)); // => -16
+console.log(multiply(19, 76)); // => 1444
+
+console.log(divide(8, 2)); // => 4
+console.log(divide(-100, 4)); // => -25
+console.log(divide(5, -3)); // => -1.6666
+
+console.log(mod(8, 7)); // => 1
+console.log(mod(12, 5)); // => 2
+console.log(mod(123, 4)); // => 3
