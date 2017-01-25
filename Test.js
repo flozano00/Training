c2dc51623749591dac8b81728cef10c5719e27ee
Frank Lozano <flozano00@gmail.com>
2017-01-25 14:42:30 -0500

    Modified the Test.js

--- Test.js
+++ Test.js
@@ -17,3 +17,27 @@ console.log(divide(5, -3)); // => -1.6666
 console.log(mod(8, 7)); // => 1
 console.log(mod(12, 5)); // => 2
 console.log(mod(123, 4)); // => 3
+
+////////////////////////////////////////////////////////
+/* I've updated it to remove the console.log due to the fact that console.log isn't needed while inserting
+in the console. */
+
+add(8,2); // => 10
+add(-5, 7); // => 2
+add(0, 28); // => 28
+
+subtract(5,3); // => 2
+subtract(-2, -7); // => 5
+subtract(132, 276); // => -144
+
+multiply(10, 12); // => 120
+multiply(-2, 8); // => -16
+multiply(19, 76); // => 1444
+
+divide(8, 2); // => 4
+divide(-100, 4); // => -25
+divide(5, -3); // => -1.6666
+
+mod(8, 7); // => 1
+mod(12, 5); // => 2
+mod(123, 4); // => 3
