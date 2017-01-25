4b22cab77adcba024a6b1a6b699dec20680c00d6
Frank Lozano <flozano00@gmail.com>
2017-01-25 14:47:15 -0500

    New folder in Test.

---
+++ Test/Test.js
@@ -0,0 +1,43 @@
+console.log(add(8,2)); // => 10
+console.log(add(-5, 7)); // => 2
+console.log(add(0, 28)); // => 28
+
+console.log(subtract(5,3)); // => 2
+console.log(subtract(-2, -7)); // => 5
+console.log(subtract(132, 276)); // => -144
+
+console.log(multiply(10, 12)); // => 120
+console.log(multiply(-2, 8)); // => -16
+console.log(multiply(19, 76)); // => 1444
+
+console.log(divide(8, 2)); // => 4
+console.log(divide(-100, 4)); // => -25
+console.log(divide(5, -3)); // => -1.6666
+
+console.log(mod(8, 7)); // => 1
+console.log(mod(12, 5)); // => 2
+console.log(mod(123, 4)); // => 3
+
+////////////////////////////////////////////////////////
+/* I've updated it to remove the console.log due to the fact that console.log isn't needed while inserting
+in the console. */
+
+add(8,2); // => 10
+add(-5, 7); // => 2
+add(0, 28); // => 28
+
+subtract(5,3); // => 2
+subtract(-2, -7); // => 5
+subtract(132, 276); // => -144
+
+multiply(10, 12); // => 120
+multiply(-2, 8); // => -16
+multiply(19, 76); // => 1444
+
+divide(8, 2); // => 4
+divide(-100, 4); // => -25
+divide(5, -3); // => -1.6666
+
+mod(8, 7); // => 1
+mod(12, 5); // => 2
+mod(123, 4); // => 3
