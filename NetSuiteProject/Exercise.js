43783c9cc301d53b3dc2c7e2e0193cd8d1bcf815
Frank Lozano <flozano00@gmail.com>
2017-01-30 13:32:05 -0500

    One function

--- NetSuiteProject/Exercise.js
+++ NetSuiteProject/Exercise.js
@@ -1,39 +1,34 @@
 //Add
-function add(x,y){
-	return x + y;
-};
+
 
 function addition(x,y){
-	if(typeof add (x,y) === "string"){
-	alert("This function only accepts Integers")
+	var add = (x + y);
+	if(typeof add === "string"){
+	alert("This function only accepts Numbers");
 	}
-	else {return x + y;}
+	else {return add;}
 }
 
 
 //subtract
-function subtract(x, y){
-	return (x - y);
-};
 
 function subtraction(x, y){
-	if(isNaN(subtract(x,y))  === true){
-	alert("This function only accepts Integers");
+	var subtract = (x - y);
+	if(isNaN(subtract) === true){
+	alert("This function only accepts Numbers");
 	}
-	else {return (x - y);}
+	else {return subtract;}
 }
 
 
 //Multiplication
-function multiply(x, y){
-	return (x * y);
-};
 
 function multiplication(x,y){
-	if(isNaN(multiply(x,y)) === true ){
-		alert("This function only accepts Integers");
+	var multiply = (x * y);
+	if(isNaN(multiply) === true ){
+		alert("This function only accepts Numbers");
 	}
-	else {return (x * y);}
+	else {return multiply;}
 }
 
 
@@ -44,22 +39,20 @@ function divide (x, y){
 
 function division(x, y){
 	if(isNaN(divide(x,y)) === true){
-		alert("This function only accepts Integers");
+		alert("This function only accepts Numbers");
 	}
 	else {return (x / y);}
 };
 
 
 //Modulus(Division Remainder)
-function mod(x, y){
-	return x % y;
-};
 
 function remainder(x ,y){
-	if (isNaN(mod(x,y)) === true){
-		alert("This function only accepts Integers");
+	var mod = (x % y);
+	if (isNaN(mod) === true){
+		alert("This function only accepts Numbers");
 	}
-	else {return (x % y);}
+	else {return mod;}
 };
 
 
