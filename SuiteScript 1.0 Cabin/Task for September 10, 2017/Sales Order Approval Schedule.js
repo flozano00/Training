0335ff6d59a09f37648aeb717985d76191de7e52
Frank Lozano <flozano00@gmail.com>
2017-09-12 16:34:39 -0400

    Tuesday, September 12 2017

--- SuiteScript 1.0 Cabin/Task for September 10, 2017/Sales Order Approval Schedule.js
+++ SuiteScript 1.0 Cabin/Task for September 10, 2017/Sales Order Approval Schedule.js
@@ -1,29 +1,54 @@
-/**
- * How do you scan each Scheduled Sales Order?
- */
-
 
 
 
 function salesOrderApproval_schedule(type){
-nlapiLogExecution("Audit", "Sales Order Approval Schedule", "Begin");
-	try{
 
+	try{
+nlapiLogExecution("Audit", "Sales Order Approval Schedule", "Begin");
 
 var recordType = nlapiGetRecordType();
 var recordId = nlapiGetRecordId();
 
 nlapiLogExecution("Debug", "The Record Type is: ", "Record Type" + recordType);
-nlapiLogExecution("Debug"vvvvvvvvvvvvvvvvvvvvv, "The Record Id is: ", "The Record ID: " + recordId);
-var salesObject = nlapiGetNewRecord();
-nlapiLogExecution("Debug", "The Sales Object is ", "The Sales Object" + salesObject);
+nlapiLogExecution("Debug", "The Record Id is: ", "The Record ID: " + recordId);
+
+
+var salesObj = nlapiLoadRecord(recordType, recordId);
+nlapiLogExecution("Debug", "The Sales Object is ", "The Sales Object" + salesObj);
+var salesStatus = salesObj.getFieldText('orderstatus');
+nlapiLogExecution('Debug', 'Sales Status', 'The Sales Status : ' + salesStatus);
+nlapiLogExecution('Emergency', 'Status Change', 'Begin');
+if (salesStatus == "Pending Approval"){
+//Issue is that you we were using setFieldValue
+//Make sure to use setFieldText if this issue persist.
+//nlapiSetFieldText('orderstatus', 'Pending Fulfillment');
+	salesObj.setFieldText('orderstatus', 'Pending Fulfillment');
+	var approved = salesObj.getFieldValue('orderstatus')
+//Always make sure to get the field value of the approved before doing log executions!
+	nlapiLogExecution('Debug', 'Sales Status Change: ', "The Approved Status " + approved);
+	nlapiSubmitRecord(salesObj, true, true);
+	//nlapiLogExecution('Debug', 'Sales Status Change: ', "The Approved Status " + approved)
+}
+
+
+nlapiLogExecution('Emergency', 'Status Change', 'End');
+
+
+
+
+
 
 
 
 
 nlapiLogExecution("Audit", "Sales Order Approval Schedule", "End");
+
 	}
 	catch(exception){
-	nlapiLogExecution("Emergency", "The Emergency exception is: ", "The exception is " + exception);
+nlapiLogExecution("Emergency", "The Emergency exception is: ", "The exception is " + exception);
 	}
 }
+
+
+
+
