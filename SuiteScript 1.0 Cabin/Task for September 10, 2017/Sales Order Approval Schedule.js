f8646cf83f2aa1d305dd2839df9cc4402b1d9959
Frank Lozano <flozano00@gmail.com>
2017-09-12 13:35:11 -0400

    README.MD

--- SuiteScript 1.0 Cabin/Task for September 10, 2017/Sales Order Approval Schedule.js
+++ SuiteScript 1.0 Cabin/Task for September 10, 2017/Sales Order Approval Schedule.js
@@ -8,14 +8,15 @@
 function salesOrderApproval_schedule(type){
 nlapiLogExecution("Audit", "Sales Order Approval Schedule", "Begin");
 	try{
+
+
 var recordType = nlapiGetRecordType();
 var recordId = nlapiGetRecordId();
 
 nlapiLogExecution("Debug", "The Record Type is: ", "Record Type" + recordType);
-nlapiLogExecution("Debug", "The Record Id is: ", "The Record ID: " + recordId);
-var salesObject = nlapiLoadRecord(recordType, recordId);
+nlapiLogExecution("Debug"vvvvvvvvvvvvvvvvvvvvv, "The Record Id is: ", "The Record ID: " + recordId);
+var salesObject = nlapiGetNewRecord();
 nlapiLogExecution("Debug", "The Sales Object is ", "The Sales Object" + salesObject);
-
 
 
 
