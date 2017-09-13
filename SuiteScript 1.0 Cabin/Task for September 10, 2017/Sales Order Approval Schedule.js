03c46f53bc00043d9b0718369bdb6b06bd21fce6
Frank Lozano <flozano00@gmail.com>
2017-09-13 14:21:21 -0400

    Sales Order Approval Schedule has been updated

--- SuiteScript 1.0 Cabin/Task for September 10, 2017/Sales Order Approval Schedule.js
+++ SuiteScript 1.0 Cabin/Task for September 10, 2017/Sales Order Approval Schedule.js
@@ -1,23 +1,61 @@
-
-
-
-function salesOrderApproval_schedule(type){
-
+function salesOrder_Approval(type){
 	try{
-nlapiLogExecution("Audit", "Sales Order Approval Schedule", "Begin");
-
+nlapiLogExecution('Audit', 'Sales Order Approval Fuction', 'Begin');
 var recordType = nlapiGetRecordType();
 var recordId = nlapiGetRecordId();
 
+
+
 nlapiLogExecution("Debug", "The Record Type is: ", "Record Type" + recordType);
 nlapiLogExecution("Debug", "The Record Id is: ", "The Record ID: " + recordId);
-
-
-var salesObj = nlapiLoadRecord(recordType, recordId);
-nlapiLogExecution("Debug", "The Sales Object is ", "The Sales Object" + salesObj);
-var salesStatus = salesObj.getFieldText('orderstatus');
-nlapiLogExecution('Debug', 'Sales Status', 'The Sales Status : ' + salesStatus);
 nlapiLogExecution('Emergency', 'Status Change', 'Begin');
+
+var salesOrderSearch = nlapiSearchRecord("salesorder",null,
+//Modify this portion at the bottom if the details show null.
+[
+   ["type","anyof","SalesOrd"], 
+   "AND", 
+   ["status","anyof","SalesOrd:A"], 
+   "AND", 
+   ["mainline","is","T"]
+], 
+[
+   new nlobjSearchColumn("internalid",null,null)
+]
+);
+
+/**
+ * If your value is 	Debug	Saved Search	9/13/2017	8:56 am	Frank Lozano	Saved Search Details: null
+ * Then make sure to edit your filters due to the fact that the results aren't showing.
+ */
+nlapiLogExecution('Debug', 'Saved Search', 'Saved Search Details: ' + JSON.stringify(salesOrderSearch));
+
+var salesId = new Array();
+
+
+for (var i = 0; i < salesOrderSearch.length; i++){
+	//I only want the internal Id Field Results and pushing it into an array.
+	//This is the propery way of pushing the array make sure the [i] is next to salesOrderSearch
+/**
+ *  ------------------------------IMPORTANT CONCEPT ------------------------------------------------------------
+ */
+	salesId.push(salesOrderSearch[i].getValue('internalid'));
+}
+nlapiLogExecution('Debug', 'Sales Id for loop', 'Sales Id Result: ' + JSON.stringify(salesId));
+
+var salesRecordID = salesId;
+nlapiLogExecution('Debug', 'Dynamic Sales Record ID', 'The Sales Record ID is' + salesRecordID);
+
+
+
+
+
+// Change the record ID with the Sales Record ID that relates to the saved search.
+var salesObj = nlapiLoadRecord(recordType, recordId);
+nlapiLogExecution("Debug", "The Sales Object is ", "The Sales Object " + salesObj);
+var salesStatus = salesObj.getFieldText('orderstatus');
+nlapiLogExecution('Debug', 'Sales Status', 'The Sales Status : ' + JSON.stringify(salesStatus));
+
 if (salesStatus == "Pending Approval"){
 //Issue is that you we were using setFieldValue
 //Make sure to use setFieldText if this issue persist.
@@ -28,27 +66,20 @@ if (salesStatus == "Pending Approval"){
 	nlapiLogExecution('Debug', 'Sales Status Change: ', "The Approved Status " + approved);
 	nlapiSubmitRecord(salesObj, true, true);
 	//nlapiLogExecution('Debug', 'Sales Status Change: ', "The Approved Status " + approved)
+	nlapiLogExecution("Audit", "Sales Order Approval Schedule", "End");
 }
+
+
+
+
 
 
 nlapiLogExecution('Emergency', 'Status Change', 'End');
 
 
 
-
-
-
-
-
-
-nlapiLogExecution("Audit", "Sales Order Approval Schedule", "End");
-
 	}
 	catch(exception){
-nlapiLogExecution("Emergency", "The Emergency exception is: ", "The exception is " + exception);
+		nlapiLogExecution('Debug', 'Exception: ', "The Exception is " + exception);
 	}
 }
-
-
-
-
